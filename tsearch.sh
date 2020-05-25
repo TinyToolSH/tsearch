db2ef802523060c67294a0cf0d70718a1269794a
gabo <gabriel.gaboardi13@gmail.com>
2020-05-24 21:47:44 -0300

    added option to get values by especifing path [tsearch] implemeted yaml config file

--- tsearch.sh
+++ tsearch.sh
@@ -1,55 +1,35 @@
 #!/bin/bash 
 
-CONFIG=~/Scripts/pytools/tsearch/list.config
+search_params=~/Scripts/pytools/tsearch/params.yaml
 
-CONFIG=~/Scripts/pytools/tsearch/list.config
+yaml_parser=$HOME/Scripts/pytools/tyaml/parse_yaml.sh
 
-# DEFAULT SEARCH PARAMS
-PROGRAM=$(awk '/^default/ && $2=="program" {print $3}' $CONFIG)
-ENGINE=$(awk '/^default/ && $2=="engine" {print $3}' $CONFIG)
-LANG="en"
+program=$($yaml_parser $search_params default_program)
+engine=$($yaml_parser $search_params default_engine)
+
+lang="en"
 
 while getopts p:e:t:l option
 do
-<<<<<<< found
 case "${option}"
 in
-p) PROGRAM=${OPTARG};;
-e) ENGINE=${OPTARG};;
-t) RAW_TERM=${OPTARG};;
-l) LANG=${OPTARG}
+p) program=${OPTARG};;
+e) engine=${OPTARG};;
+t) raw_term=${OPTARG};;
+l) lang=${OPTARG}
 esac
 done
 
-TERM=${RAW_TERM// /+}
+term=${raw_term// /+}
 
-create_query(){
-    QUERY=$(awk -v selected_engine="$1" '/^engine/ && $2==selected_engine {print $3}' $CONFIG)
-    echo "${QUERY/TERM/$TERM}"
-||||||| expected
-
-create_query(){
-    case "$1" in
-        duckduckgo) QUERY="https://www.duckduckgo.com/?q=$TERM" ;;
-        google) QUERY="https://www.google.com/search?q=$TERM" ;;
-        youtube) QUERY="https://www.youtube.com/results?search_query=$TERM" ;;
-        pacman) QUERY="sudo pacman -S $RAW_TERM" ;;
-        apt) QUERY="sudo apt-get $RAW_TERM" ;;
-        aur) QUERY="yaourt $RAW_TERM" ;;
-        translate) QUERY="https://translate.google.com/?sl=auto&tl=$LANG&text=$TERM" ;;
-        # wikipedia) to be implemented
-        *) echo "ENGINE NOT SUPPORTED" ;;
-    esac
-    echo "$QUERY"
-=======
-
-create_query(){
-    QUERY=$(awk -v selected_engine="$1" '/^engine/ && $2==selected_engine {print $3}' $CONFIG)
-    echo "${QUERY/TERM/$TERM}"
->>>>>>> replacement
+get_query(){
+    query=$($yaml_parser $search_params "$engine"_query)
+    echo "${query/TERM/$term}"
 }
 
-case "$ENGINE" in
-    pacman | apt | aur) $(create_query $ENGINE) ;;
-    *) $PROGRAM $(create_query $ENGINE) ;;
-esac
+command=$($yaml_parser $search_params "$program"_command)
+
+# case "$engine" in
+    # pacman | apt | aur) $(create_query $ENGINE) ;;
+    $command $(get_query $engine)
+# esac
